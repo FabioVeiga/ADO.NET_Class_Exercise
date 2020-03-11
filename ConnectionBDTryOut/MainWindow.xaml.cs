11faf771dc5cf632231b5a5ac5f2dc7edc3c8d67
FabioVeiga <droidbinho@gmail.com>
2020-03-10 20:47:31 -0400

    Commit

---
+++ ConnectionBDTryOut/MainWindow.xaml.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ConnectionBDTryOut
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadStudent();
+            DeleteButton.IsEnabled = true;
+            UpdateButton.IsEnabled = true;
+        }
+
+        private void LoadStudent()
+        {
+            StudentList.Items.Clear();
+            Student student = new Student();
+            for (int i = 0; i < student.ListOfStudent().Count; i++)
+            {
+                StudentList.Items.Add(student.ListOfStudent()[i]);
+            }
+        }
+
+        private void StudentList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Student student = new Student();
+            int aux = StudentList.SelectedItem.ToString().IndexOf("-", 0);
+            int Id = int.Parse(StudentList.SelectedItem.ToString().Substring(0, aux).ToString().Trim());
+
+            student = student.getStudent(Id);
+
+            IDText.Text = student.Id.ToString();
+            FirstNameText.Text = student.FirstName.ToString();
+            LastNameText.Text = student.LastName.ToString();
+
+            DeleteButton.IsEnabled = true;
+            UpdateButton.IsEnabled = true;
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            Student student = new Student();
+            int aux = StudentList.SelectedItem.ToString().IndexOf("-", 0);
+            int Id = int.Parse(StudentList.SelectedItem.ToString().Substring(0, aux).ToString().Trim());
+            if (student.DeleteStudent(Id))
+            {
+                MessageBox.Show("Student has been deleted!", "Delete");
+            }
+            else
+            {
+                MessageBox.Show("Something goes wrong, try again!", "Delete");
+            }
+        }
+
+        private void UpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            Student student = new Student();
+
+            student.Id = int.Parse(IDText.Text);
+            student.FirstName = FirstNameText.Text;
+            student.LastName = LastNameText.Text;
+
+            if (student.UpdateStudent(student))
+            {
+                MessageBox.Show("Student has been updated!", "Delete");
+            }
+            else
+            {
+                MessageBox.Show("Something goes wrong, try again!", "Delete");
+            }
+        }
+
+        private void InsertButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(IDText.Text != "")
+            {
+                DeleteButton.IsEnabled = false;
+                UpdateButton.IsEnabled = false;
+                FirstNameText.Focus();
+                IDText.Text = "";
+                FirstNameText.Text = "";
+                LastNameText.Text = "";
+            }
+            else
+            {
+                Student student = new Student();
+                student.FirstName = FirstNameText.Text;
+                student.LastName = LastNameText.Text;
+                if(FirstNameText.Text == "" && LastNameText.Text == "")
+                {
+                    MessageBox.Show("Please, Fill the gaps", "Insert");
+                }
+                else
+                {
+                    student.InsertStudent(student);
+                    MessageBox.Show("Student has been inserted", "Insert");
+                }
+                
+            }
+        }
+    }
+}
---
+++ ConnectionBDTryOut/Student.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ConnectionBDTryOut
+{
+    class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        private string connectionStr = "Server=INSTRUCTORIT;Database=IBTCollege;User Id=ProfileUser;Password=ProfileUser2019";
+
+        public Student(){}
+
+        public List<string> ListOfStudent()
+        {
+            List<string> students = new List<string>();
+
+            using (SqlConnection conn = new SqlConnection(connectionStr))
+            {
+                string sql = "SELECT Id, firstName, lastName FROM Students";
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    students.Add(reader["id"] + " - " + reader["firstName"] + " " + reader["lastName"]);
+                }
+                reader.Close();
+            }
+            return students;
+        }
+
+        public Student getStudent(int id)
+        {
+            Student student = new Student();
+            using (SqlConnection conn = new SqlConnection(connectionStr))
+            {
+                string sql = "SELECT Id, firstName, lastName FROM Students WHERE Id=" + id;
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                DataTable dt = new DataTable();
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                adapter.Fill(dt);
+
+                if (dt.Rows.Count > 0)
+                {
+                    student.Id = int.Parse(dt.Rows[0]["Id"].ToString());
+                    student.FirstName = dt.Rows[0]["firstName"].ToString();
+                    student.LastName = dt.Rows[0]["lastName"].ToString();
+                }
+
+            }
+            return student;
+        }
+
+        public bool DeleteStudent(int id)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionStr))
+            {
+                conn.Open();
+                string sql = "DELETE FROM Students WHERE id = " + id;
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                if(cmd.ExecuteNonQuery() != 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public bool UpdateStudent(Student student)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionStr))
+            {
+                conn.Open();
+                string sql = "UPDATE Students SET " +
+                    "firstName = '" + student.FirstName + "'," +
+                    "lastName = '" + student.LastName + "' " +
+                    "WHERE id = " + student.Id;
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                if (cmd.ExecuteNonQuery() != 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public bool InsertStudent(Student student)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionStr))
+            {
+
+                conn.Open();
+                string sql = "INSERT INTO Students (firstName, lastName) VALUES(" +
+                    "'" + student.FirstName + "'," +
+                    "'" + student.LastName + "')";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                if (cmd.ExecuteNonQuery() != 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+    }
+}
